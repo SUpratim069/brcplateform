c681709a7b0456b42b5a06de3eb1c7fe3e63b380
vidyantsharma5519 <vidyantkumar5519@gmail.com>
2025-08-30 13:41:48 +0530

    added

--- api/checkout.js
+++ api/checkout.js
@@ -34,5 +34,6 @@ export default async function handler(req, res) {
   } catch (err) {
     console.error("Stripe error:", err);
     res.status(500).json({ error: err.message });
-  }
-}
+  } 
+} 
+//api
